1694d6a5bd7c5cc6abf8ff134a2b5c333cd673a4
league <league@adminisorsimac3.attlocal.net>
2025-02-20 17:00:16 -0800

    2/20/25 Today I did a little bit of chuckle clicker.

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,14 @@
+package _01_chuckle_clicker;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChuckleClicker {
+public static void main(String[] args) {
+	
+}
+void makeButtons() {
+	JFrame platform = new JFrame();
+	JPanel blah = new JPanel();
+}
+}
