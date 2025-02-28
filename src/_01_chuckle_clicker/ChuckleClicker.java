31b099b2ff16153c72c99b2fc9cb95269d5d14b3
league <league@adminisorsimac3.attlocal.net>
2025-02-27 17:02:04 -0800

    2/27/25 Today, I did chuckle clicker and jeopardy.

--- src/_01_chuckle_clicker/ChuckleClicker.java
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -1,14 +1,55 @@
 package _01_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
-public static void main(String[] args) {
+import _03_gui_with_help._2_photo_quiz.PhotoQuiz;
+
+public class ChuckleClicker implements ActionListener {
+	JFrame platform = new JFrame("Make Buttons");
+	JPanel chucklePanel = new JPanel();
+	JButton joke = new JButton("Joke");
+	JButton punchline = new JButton("Punchline");
 	
-}
-void makeButtons() {
-	JFrame platform = new JFrame();
-	JPanel blah = new JPanel();
-}
+	
+	
+	public static void main(String[] args) {
+		new ChuckleClicker().makeButtons();
+	}
+	
+	
+	
+	
+
+	void makeButtons() {
+		platform.add(chucklePanel);
+		chucklePanel.add(joke);
+		chucklePanel.add(punchline);
+		punchline.addActionListener(this);
+		joke.addActionListener(this);
+		platform.pack();
+		platform.setVisible(true);
+		
+//if this object of this class is the action listener, then why do we need the variable JButton to access the events/messages sent by the buttons?	
+		
+		
+
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if(buttonPressed == joke) {
+			JOptionPane.showMessageDialog(null,  "Do you know why the tailor got fired?");
+		}
+//the tailor got fired, he just wasn't a good fit
+		if(buttonPressed == punchline) {
+			JOptionPane.showMessageDialog(null, "He just wasn't a good fit!");
+		}
+	}
 }
